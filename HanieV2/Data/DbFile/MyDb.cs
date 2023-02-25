4a00d29cad5ba88cc610347889840dbea4988cb1
Mehrshad <lastenigmas@gmail.com>
2023-02-25 10:56:29 +0330

    Add project files.

---
+++ HanieV2/Data/DbFile/MyDb.cs
@@ -0,0 +1,15 @@
+﻿using HanieV2.Data.Model.PaintsObj;
+using Microsoft.EntityFrameworkCore;
+
+namespace HanieV2.Data.DbFile
+{
+    public class MyDb : DbContext
+    {
+        public MyDb( DbContextOptions<MyDb> options ) : base( options )
+        {
+
+        }
+
+        public DbSet<Paint> Paints { get; set; }
+    }
+}
---
+++ HanieV2/Data/Model/PaintsObj/Paint.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HanieV2.Data.Model.PaintsObj
+{
+    public class Paint
+    {
+        [Key]
+        public int PaintId { get; set; }
+        public int MyPaintId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public string PaintImageName { get; set; }
+    }
+}
